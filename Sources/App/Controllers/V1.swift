09b047edea4f4dbcf72ddff7158ea7f55a5e5f22
Mugurel Moscaliuc <yakis@Mugurels-iMac.local>
2017-11-08 14:16:07 +0000

    Vapor updated

--- Sources/App/Controllers/V1.swift
+++ Sources/App/Controllers/V1.swift
@@ -12,14 +12,11 @@ class V1: RouteCollection {
     
     func build(_ builder: RouteBuilder) throws {
         let v1 = builder.grouped("api", "v1")
-        let middleware = FirebaseAuthMiddleware(with: "mytrax-47920")
-        let secured = v1.grouped(middleware)
         
-        let tracks = secured.grouped("tracks")
         
         
         let users = v1.grouped("users")
-       // let tracks = v1.grouped("tracks")
+        let tracks = v1.grouped("tracks")
         let posts = v1.grouped("posts")
         let postComments = v1.grouped("postcomments")
         let postreplies = v1.grouped("postreplies")
